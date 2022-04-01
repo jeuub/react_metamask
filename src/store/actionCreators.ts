fa28944179fb5df97820d1b48da61b6f654fe404
jeuub <71319621+jeuub@users.noreply.github.com>
2022-04-01 03:57:41 +0300

    web 3 connect

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -1,14 +1,31 @@
 import { AppDispatch } from "./store"
 import { AuthSlice } from "./Slices/AuthSlice";
+import Web3 from "web3";
+declare var window: any;
+
+const detectedProvider = () => {
+  let provider;
+  if (window.etherium) {
+    provider = window.ethereum
+  } else if (window.web3) {
+    provider = window.web3.currentProvider;
+  } else {
+    provider = null;
+  }
+  return provider
+}
 
 export const requestAccounts = () => async (dispatch: AppDispatch) => {
   try {
     dispatch(AuthSlice.actions.userFetching);
-    let provider = window.ethereum;
-    const data = await provider.request({
-      method: 'eth_requestAccounts'
-    });
-    dispatch(AuthSlice.actions.userFetchedSuccess(data))
+    let provider = detectedProvider();
+    console.log(provider);
+    const web3 = new Web3(provider);
+    const accounts = await web3.eth.getAccounts()
+    if (accounts.length === 0) {
+      throw new Error('Please connect to Metamask')
+    }
+    dispatch(AuthSlice.actions.userFetchedSuccess(accounts))
   }
   catch (e) {
     if (typeof e === "string") {
